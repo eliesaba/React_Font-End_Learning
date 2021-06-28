59da6851eabb856e17695b5ed3d7ad6d38337edb
eliesaba <elie.bs.15@hotmail.com>
2021-06-28 12:31:59 +0200

    Application code testing

---
+++ newapp/src/App.js
@@ -0,0 +1,99 @@
+import logo from './logo.svg';
+import React, { useState } from 'react';
+import {FaStar} from "react-icons/fa";
+import './App.css';
+import { Component } from 'react';
+import ColorData from './color-data.json'
+import {ColorList} from './ColorList.js'
+
+// export const data = [
+//   { 
+//    name: "Baked Salmon",
+//    ingredients : ["Salmon", "Pine Nuts", "Butter Lettuce"],
+//    steps: ["Preheat the oven to 350 degrees", "Spread the olive oil", "Add the yellow squash"]
+//   },
+//   { 
+//     name: "Fish Tacos",
+//     ingredients : ["Whitefish", "Cheese", "Iceberg Letttuce"],
+//     steps: ["Cook the fish on grill", "place the fish", "Top them with lettuce"]
+//    }
+// ];
+
+// export function Menu ({title, array}) {
+//   return(
+//     <section>
+//       <h1>{title}</h1>
+//       {array.map((item, i)=> (
+//         <React.Fragment>
+//           <Ingredients key={i} {...item} />
+//           <Steps key={i} {...item} />
+//         </React.Fragment>
+//       ))}
+//      </section> 
+//   );
+// }
+
+// export function Ingredients ({ingredients, name}) {
+//   return(
+//   <section>  
+//     <h3>{name}</h3>
+//     <ul>
+//       {ingredients.map((item, i)=> (
+//         <li>{item}</li>
+//       ))}
+//      </ul>
+//    </section>   
+//   );
+// }
+
+// export function Steps ({steps}) {
+//   return(
+//   <section>  
+//       {steps.map((item, i)=> (
+//         <p>{item}</p>
+//       ))}
+     
+//    </section>   
+//   );
+// }
+
+// export function StarRating() {
+//   return[
+//   <React.Fragment>
+//     <FaStar color="red" />
+//     <FaStar color="red" />
+//     <FaStar color="red" />
+//     <FaStar color="grey" />
+//     <FaStar color="grey" />
+//   </React.Fragment>  
+//   ];
+// }
+
+// const Star = ({selected = false, onSelect = f => f})=> (
+//   <FaStar color={selected? "red" : "grey"} onClick={onSelect}/>
+// );
+
+// export const createArray = length => [...Array(length)];
+
+// export function CreateStar ({numberOfStars = 5, ClickStar = 0,  onClickStar = f => f }) {
+  
+//      return (
+//      <div>  
+//      {createArray(numberOfStars).map((item,i) => <Star key= {i}  selected={ClickStar > i}
+//                                                                  onSelect={()=>{onClickStar(i+1)}}/>)}
+//      <p>{ClickStar} of {numberOfStars}</p>
+//      </div>
+//      );
+//     }
+
+// export function App() {
+//   const [Colors, setColors] = useState(ColorData);
+//   return <ColorList colors= {Colors} onRemoveColor={id => {
+//                                 const newColors = Colors.filter(color => color.id !== id);
+//                                 setColors(newColors);}}
+//                               onClickStarColor = {(id,rating) => {
+//                                 const newColors = Colors.map(color => color.id === id ? {...color,rating} : color);
+//                                 setColors(newColors);
+//                                       } }/>
+// }
+
---
+++ newapp/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import {data} from './App';
+//import {Menu} from './App';
+import { StarRating } from './App';
+import { CreateStar } from './App';
+import reportWebVitals from './reportWebVitals';
+import { ColorList } from './ColorList';
+import { App } from './App';
+
+ReactDOM.render(
+  //<Menu array= {data} title= "Delicious Recipes"/>,document.getElementById('root')
+  //<CreateStar/>,document.getElementById('root')
+   <App />, document.getElementById('root')
+);
